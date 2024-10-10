9a577d0168b8dba04826530dc145950d1d55f0d8
TimBlokdijk <1026135@mborijnland.nl>
2024-10-10 12:16:27 +0200

    added includes

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+
+
+<header>
+        <div class="logo">
+            <img src="images/logo.png" alt="Company Logo">
+            
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">Diensten</a></li>
+                <li><a href="#">Blog/Nieuws</a></li>
+                <li><a href="#">Account</a></li>
+            </ul>
+        </nav>
+</header>
+
