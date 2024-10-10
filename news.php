882e4e5343ca63e34723e79215b2cee70a6a8727
TimBlokdijk <1026135@mborijnland.nl>
2024-10-11 00:06:41 +0200

    Made the main for news.php

--- news.php
+++ news.php
@@ -11,7 +11,21 @@
     <?php require 'includes/header.php'; ?>
 
     <main id="news_main">
-
+        <section>
+            <div><img src="images/logo.png" alt="img"></div>
+            <h2>heading</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut quidem consequatur incidunt perspiciatis! Sit ad soluta vero incidunt esse necessitatibus officiis ullam, porro similique a, explicabo voluptates sunt tempora? Itaque!</p>
+        </section>
+        <section>
+            <div><img src="images/logo.png" alt="img"></div>
+            <h2>heading</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptatibus laboriosam voluptatum. Tenetur perferendis ullam temporibus laudantium veritatis. Tenetur distinctio reprehenderit dolore voluptatibus, totam cum esse voluptates culpa sint quos.</p>
+        </section>
+        <section>
+            <div><img src="images/logo.png" alt="img"></div>
+            <h2>heading</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam fuga minima reprehenderit dolorem deleniti assumenda eius, voluptatibus delectus? Doloremque inventore deserunt tempora rem itaque consectetur corrupti nemo qui earum? Dolorem!</p>
+        </section>
     </main>
 
     <?php require 'includes/footer.php'; ?>
