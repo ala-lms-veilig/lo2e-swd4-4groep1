95b4e5f835a7756a969f4232415e37e902843460
TimBlokdijk <1026135@mborijnland.nl>
2024-10-18 10:42:15 +0200

    news json code

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
     <main id="contact_main">
         <template id="contact_template">
             <section>
-                <div><img class="contact_img" src="images/logo.png" alt="img"></div>
+                <div><img class="contact_img" src="" alt="img"></div>
                 <article>
                     <div>
                         <h4>Beroep:</h4>
@@ -30,9 +30,6 @@
                 </article>
             </section>
         </template>
-
-        <div id="container"></div>
-
     </main>
 
     <?php require 'includes/footer.php'; ?>
@@ -44,13 +41,12 @@
     // http://localhost/Github_PHP/lo2e-swd4-4groep1/db.json
     async function showContactInfo() {
         const response = await fetch(`https://my-json-server.typicode.com/ala-lms-veilig/lo2e-swd4-4groep1/contacts`);
-
         const contacts = await response.json();
 
         console.log(contacts);
 
         const template = document.getElementById("contact_template");
-        const container = document.getElementById("container");
+        const container = document.getElementById("contact_main");
 
         // If server doesn't work use contacts.contacts
         for (let contact of contacts) {
@@ -61,7 +57,7 @@
             const naam      = clone.querySelector(".contact_naam");
             const telNummer = clone.querySelector(".contact_tel");
 
-            img.innerHTML       = `images/${contact.img}`;
+            img.src        = `images/${contact.img}`;
             beroep.innerHTML    = contact.beroep;
             naam.innerHTML      = contact.naam;
             telNummer.innerHTML = contact.telNummer;
--- news.php
+++ news.php
@@ -13,24 +13,46 @@
     <!-- Replace the sections with one template that gets data from db.json -->
 
     <main id="news_main">
+    <template id="news_template">
         <section>
-            <div><img src="images/logo.png" alt="img"></div>
-            <h2>heading</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut quidem consequatur incidunt perspiciatis! Sit ad soluta vero incidunt esse necessitatibus officiis ullam, porro similique a, explicabo voluptates sunt tempora? Itaque!</p>
+            <div><img class="news_img" src="" alt="img"></div>
+            <h2 class="news_title">heading</h2>
+            <p class="news_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut quidem consequatur incidunt perspiciatis! Sit ad soluta vero incidunt esse necessitatibus officiis ullam, porro similique a, explicabo voluptates sunt tempora? Itaque!</p>
         </section>
-        <section>
-            <div><img src="images/logo.png" alt="img"></div>
-            <h2>heading</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptatibus laboriosam voluptatum. Tenetur perferendis ullam temporibus laudantium veritatis. Tenetur distinctio reprehenderit dolore voluptatibus, totam cum esse voluptates culpa sint quos.</p>
-        </section>
-        <section>
-            <div><img src="images/logo.png" alt="img"></div>
-            <h2>heading</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam fuga minima reprehenderit dolorem deleniti assumenda eius, voluptatibus delectus? Doloremque inventore deserunt tempora rem itaque consectetur corrupti nemo qui earum? Dolorem!</p>
-        </section>
-    </main>
+    </template>
+</main>
 
     <?php require 'includes/footer.php'; ?>
 
 </body>
 </html>
+
+<script>
+    // http://localhost/Github_PHP/lo2e-swd4-4groep1/db.json
+    async function showNewsInfo() {
+        const response = await fetch(`https://my-json-server.typicode.com/ala-lms-veilig/lo2e-swd4-4groep1/news`);
+        const newsInfo = await response.json();
+
+        console.log(newsInfo);
+
+        const template = document.getElementById("news_template");
+        const container = document.getElementById("news_main");
+
+        // If server doesn't work use news.news
+        for (let news of newsInfo) {
+            const clone = template.content.cloneNode(true);
+
+            const img       = clone.querySelector(".news_img");
+            const title     = clone.querySelector(".news_title");
+            const text      = clone.querySelector(".news_text");
+
+            img.src             = `images/${news.img}`;
+            title.innerHTML     = news.title;
+            text.innerHTML      = news.txt;
+
+            container.appendChild(clone);
+        }
+    }
+
+    showNewsInfo();
+</script>
