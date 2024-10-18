b8012a4e176c9930b7069553a8a1fd9de24ead64
TimBlokdijk <1026135@mborijnland.nl>
2024-10-18 09:41:08 +0200

    Testing .Json connection

--- contact.php
+++ contact.php
@@ -31,7 +31,7 @@
             </section>
         </template>
 
-        <div id="container"></div> <!-- Fixed the ID from 'contact_container' to 'container' -->
+        <div id="container"></div>
 
     </main>
 
@@ -44,6 +44,12 @@
 
     async function showContactInfo() {
         const response = await fetch(`https://my-json-server.typicode.com/ala-lms-veilig/lo2e-swd4-4groep1/contacts`);
+
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+        }
+
         const contacts = await response.json();
 
         console.log(contacts);
