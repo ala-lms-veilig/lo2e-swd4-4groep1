fb7318007efb356ae85a4fc64a0903489c8c5852
TimBlokdijk <1026135@mborijnland.nl>
2024-10-21 14:24:58 +0200

    news admin table and form updated

--- news_admin.php
+++ news_admin.php
@@ -11,18 +11,32 @@
     <?php require 'includes/header.php'; ?>
 
 <main id="news_admin_main">
-        <table>
-            <tr>
-                <th>id</th>
-                <th>image</th>
-                <th>text</th>
-            </tr>
-            <tr>
-                <td>NULL</td>
-                <td>NULL</td>
-                <td>NULL</td>
-            </tr>
-        </table>
+    <form action="">
+        <article>
+            <input type="text">
+            <input type="text">
+        </article>
+        <article>
+            <button>Cancle</button>
+            <button>OK</button>
+        </article>
+    </form>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>image</th>
+            <th>text</th>
+            <th>Update</th>
+            <th>Delete</th>
+        </tr>
+        <tr>
+            <td>id_info</td>
+            <td>img_url</td>
+            <td>text_info</td>
+            <td><button class="update-btn">Update</button></td>
+            <td><button class="delete-btn">Delete</button></td>
+        </tr>
+    </table>
 </main>
 
     <?php require 'includes/footer.php'; ?>
