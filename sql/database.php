9e032aa5a8e6ea7029ce76455ee9e5c6ce85abc5
sjouk jonker <sjoukjonker13@gmail.com>
2024-11-12 14:54:14 +0100

    s

---
+++ sql/database.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Welkom01";
+$dbname = "beveleging_app";
+
+// Maak verbinding
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Controleer verbinding
+if ($conn->connect_error) {
+    die("Verbinding mislukt: " . $conn->connect_error);
+}
+echo "Verbinding succesvol";
+?>
