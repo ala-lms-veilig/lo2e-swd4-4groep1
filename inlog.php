b9b1ac6129d70acbdbe874d84f3fbcffcb05c031
sjouk jonker <sjoukjonker13@gmail.com>
2024-11-12 14:49:16 +0100

    inlog met php werkt
    nu moet nog alles goed gezet worden

--- inlog.php
+++ inlog.php
@@ -5,15 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account</title>
 
-    <link rel="stylesheet" href="./styles/styling-v2.css">
+    <link rel="stylesheet" href="styles/styling-v2.css">
+    <link rel="stylesheet" href="styles/inlog.css">
     <!-- <script src="./scripts/inlogg.js"></script> -->
      <!-- ik heb de script laten staan omdat ik het nog moet laten zien aan docent  -->
 </head>
 <body>
-    <?php //  require_once 'includes/header.php'; 
-    require_once 'sql/database.php';
+    <?php   require_once 'includes/header.php'; 
+
+session_start();
+require_once 'sql/database.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
     
-    ?>
+    // Query to check the username and password
+    $stmt = $conn->prepare('SELECT username, role FROM users WHERE username = ? AND password = ?');
+    $stmt->bind_param('ss', $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    if ($user) {
+        // Store username and role in session
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
+       
+    } else {
+     "failed ";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
     
 
     <main id="inlog">
@@ -30,7 +57,7 @@
         <div id="message"></div>
     </main>
 
-    <?php //require_once 'includes/footer.php'; ?>
+    <?php require_once 'includes/footer.php'; ?>
 
 </body>
 </html>
--- sql/database.php
+++ sql/database.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "Welkom01";
 $dbname = "beveleging_app";
 
 // Maak verbinding
