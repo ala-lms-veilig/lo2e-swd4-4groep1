7988efba4316527f41c9811befc740402665b995
TimBlokdijk <1026135@mborijnland.nl>
2024-11-19 11:58:38 +0100

    Removed the styling links.
    There's only one link in the includes/header.php since that one is automatically used on every page.

--- enquêtes.php
+++ enquêtes.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Enquête over Veiligheid</title>
-    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
  
--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="styles/styling-v2.css">
 </head>
 <body class="index-body">
     <?php require_once 'includes/header.php'; ?>
