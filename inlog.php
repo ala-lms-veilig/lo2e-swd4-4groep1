3d03a447fbbe2cabffefb69260e9676044a59a27
TimBlokdijk <1026135@mborijnland.nl>
2024-11-25 13:25:55 +0100

    Update inlog.php

--- inlog.php
+++ inlog.php
@@ -5,9 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account</title>
-
-    <link rel="stylesheet" href="styles/styling-v2.css">
-    <link rel="stylesheet" href="styles/inlog.css">
+    
     <!-- <script src="./scripts/inlogg.js"></script> -->
     <!-- ik heb de script laten staan omdat ik het nog moet laten zien aan docent  -->
 </head>
